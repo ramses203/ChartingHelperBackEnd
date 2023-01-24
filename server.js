585cd5b4b27e7b8329ec58a9a0e9641546e7d71e
Johngreen <ramses203@naver.com>
2023-01-24 23:15:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require("express");
-// const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
 const app = express();
 const Depth1 = require("./routes/Depth1");
 
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-// app.use(cors());
-
 app.use("/Depth1", Depth1);
+
+// 리액트 빌드파일을 서버에 제공
+// app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
