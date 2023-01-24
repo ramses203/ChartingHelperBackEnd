eb068f92cb1566fa92a8dd4b42acc5ec06ecd4b2
Johngreen <ramses203@naver.com>
2023-01-24 17:33:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const cors = require("cors");
-const PORT = process.env.PORT || 3001;
+// const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 const app = express();
 const Depth1 = require("./routes/Depth1");
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+// app.use(cors());
 
 app.use("/Depth1", Depth1);
 
