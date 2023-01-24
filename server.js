1a6547e895aaee74c600549a35200547a59b8917
Johngreen <ramses203@naver.com>
2023-01-24 16:48:03 +0900

    test3

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const Depth1 = require("./routes/Depth1");
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/Depth1", Depth1);
 
 app.listen(PORT, () => {
